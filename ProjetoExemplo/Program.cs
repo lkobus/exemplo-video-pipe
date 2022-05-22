92e5cd0ccdab6801417ef8126ed87b9217e5af47
lkobus2 <leonardo.kobus@gmail.com>
2022-05-22 16:10:58 -0300

    Problema

--- ProjetoExemplo/Program.cs
+++ ProjetoExemplo/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetoExemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");dsadsad
         }
     }
 }
